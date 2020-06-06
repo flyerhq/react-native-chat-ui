940b58f65bcaf1a5945a553d7407dfa2ebc71cec
Alex Demchenko <alexdemchenko@yahoo.com>
2020-06-06 02:49:38 +0200

    Update dependencies

--- jest/setup.ts
+++ jest/setup.ts
@@ -1,13 +1,4 @@
-import { Dimensions, LayoutAnimation } from 'react-native'
 import * as utils from '../src/utils'
 
-jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
-
 jest.spyOn(Date, 'now').mockReturnValue(0)
-jest
-  .spyOn(Dimensions, 'get')
-  .mockImplementation(
-    jest.fn(() => ({ width: 414, height: 896, scale: 2, fontScale: 1 }))
-  )
-jest.spyOn(LayoutAnimation, 'configureNext').mockImplementation()
 jest.spyOn(utils, 'uuidv4').mockReturnValue('uuidv4')
