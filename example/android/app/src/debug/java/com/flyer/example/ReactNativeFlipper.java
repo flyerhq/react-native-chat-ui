b72037c5a67846c8f66917a47912b1c95e828d14
Alex Demchenko <alexdemchenko@yahoo.com>
2020-12-13 23:56:05 +0100

    Bump version number

--- example/android/app/src/debug/java/com/flyer/example/ReactNativeFlipper.java
+++ example/android/app/src/debug/java/com/flyer/example/ReactNativeFlipper.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
+ * <p>This source code is licensed under the Apache-2.0 license found in the LICENSE file in the root
  * directory of this source tree.
  */
 package com.flyer.example;
